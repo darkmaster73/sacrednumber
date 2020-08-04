566d2e9e2b2e9ff129e7f020bef71c4caeda19fa
darkmaster73 <awsoemalvin@gmail.com>
2020-08-04 16:55:19 -0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let quotesDiv = document.getElementById('quotes')
-
-fetch('https://api.kanye.rest/')
-.then(res => res.json())
-.then(quote => {
-    qutoesDiv.innerHTML += `<p> ${quote.quote} </p>`})
